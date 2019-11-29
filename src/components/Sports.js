6bb6f1c1ad2ce2a5ba516ad8b7c9936117bbed88
Ryo Kawakami <ryo.kawakami@miidas.jp>
2019-11-29 18:45:58 +0900

    アドセンス

--- src/components/Sports.js
+++ src/components/Sports.js
@@ -1,6 +1,6 @@
 import React from "react"
 import SportsIndex from "./SportsIndex"
-import AdSense from "react-adsense"
+// import AdSense from "react-adsense"
 import ReactGA from "react-ga"
 
 // 大枠のデザイン。広告もここでやる
@@ -21,13 +21,13 @@ class Sports extends React.Component {
     return (
       <div className={"st-Container"}>
         <SportsIndex />
-        <AdSense.Google
-          client="ca-pub-7387197978271555"
-          className="adsbygoogle"
-          slot="xxxx"
-          format="auto"
-          style={{ display: "block" }}
-        />
+        {/* <AdSense.Google */}
+        {/*  client="ca-pub-7387197978271555" */}
+        {/*  className="adsbygoogle" */}
+        {/*  slot="xxxx" */}
+        {/*  format="auto" */}
+        {/*  style={{ display: "block" }} */}
+        {/* /> */}
       </div>
     )
   }
